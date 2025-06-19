4529e3b81512d710327cf1e48a8a0f2e596b461d
ybondar <taxist.samael@gmail.com>
2025-06-19 16:42:25 +0200

    CountingDuplicates.kt - update solution

--- src/Kata/CountingDuplicates.kt
+++ src/Kata/CountingDuplicates.kt
@@ -5,7 +5,7 @@ import org.testng.annotations.Test
 
 fun duplicateCount(text: String): Int {
     val duplicateCount = HashMap<Char, Int>()
-    text.forEach { char ->
+    text.lowercase().forEach { char ->
         if (text.count { char == it } > 1) {
             val existingCount = duplicateCount[char] ?: 0
             duplicateCount[char] = existingCount + 1
@@ -24,5 +24,16 @@ class CountingDuplicatesTest {
     @Test
     fun `abcdea returns one`() {
         assertEquals(1, duplicateCount("abcdea"))
+    }
+
+    @Test
+    fun `indivisibility returns one`() {
+        assertEquals(1, duplicateCount("indivisibility"))
+    }
+
+    @Test
+    fun `really long string containing duplicates returns three`() {
+        val text = "dA" + "c".repeat(10) + "b".repeat(100) + "a".repeat(1000)
+        assertEquals(3, duplicateCount(text))
     }
 }
