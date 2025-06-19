8a90388513b67ef481c504b4b098d7c581d17aba
ybondar <taxist.samael@gmail.com>
2025-06-19 18:40:24 +0200

    SplitString.kt - best solution

--- src/Kata/SplitStrings.kt
+++ src/Kata/SplitStrings.kt
@@ -3,7 +3,10 @@ package Kata
 import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
-fun solution(s: String): List<String> {
+//best solution
+fun splitStrings2(s: String) = s.chunked(2).map { it.padEnd(2, '_') }
+
+fun splitStrings(s: String): List<String> {
     val string = if (s.isEmpty() || s.count().rem(2) == 0) s else s + "_"
     return string.chunked(2)
 }
@@ -11,8 +14,8 @@ fun solution(s: String): List<String> {
 class SolutionTest {
     @Test
     fun basicTests() {
-        assertEquals(emptyList<String>() , solution(""), "Should handle empty string")
-        assertEquals(listOf("ab", "cd", "ef"), solution("abcdef"), "Should handle even string")
-        assertEquals(listOf("ab", "cd", "ef", "g_"), solution("abcdefg"), "Should handle odd string")
+        assertEquals(emptyList<String>() , splitStrings(""), "Should handle empty string")
+        assertEquals(listOf("ab", "cd", "ef"), splitStrings("abcdef"), "Should handle even string")
+        assertEquals(listOf("ab", "cd", "ef", "g_"), splitStrings("abcdefg"), "Should handle odd string")
     }
 }
